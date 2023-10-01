cdff929de16ed7a82a6bcbe50e77bb2bdcb76991
Onkabetse <gosiameonkabetse077@gmail.com>
2023-10-01 20:40:33 +0200

    Commit

--- app/src/main/java/com/onkabetse/votesa/activities/SignUpActivity.java
+++ app/src/main/java/com/onkabetse/votesa/activities/SignUpActivity.java
@@ -3,14 +3,31 @@ package com.onkabetse.votesa.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.onkabetse.votesa.R;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class SignUpActivity extends AppCompatActivity {
 
+    private CircleImageView userProfile;
+    private EditText userName,userPassword,userEmail,userNationalID;
+    private Button signUpBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        findViewById(R.id.have_acc).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        userProfile = findViewById(R.id.profile_image);
     }
 }
