939158c0795c33732208df3e7d5c68877f6a0521
Onkabetse <gosiameonkabetse077@gmail.com>
2023-10-02 20:21:22 +0200

    Commit

--- app/src/main/java/com/onkabetse/votesa/activities/SignUpActivity.java
+++ app/src/main/java/com/onkabetse/votesa/activities/SignUpActivity.java
@@ -2,6 +2,7 @@ package com.onkabetse.votesa.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,8 +15,9 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class SignUpActivity extends AppCompatActivity {
 
     private CircleImageView userProfile;
-    private EditText userName,userPassword,userEmail,userNationalID;
+    private EditText userName, userPassword, userEmail, userNationalID;
     private Button signUpBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,5 +31,25 @@ public class SignUpActivity extends AppCompatActivity {
         });
 
         userProfile = findViewById(R.id.profile_image);
+        userName = findViewById(R.id.user_name);
+        userPassword = findViewById(R.id.user_password);
+        userEmail = findViewById(R.id.user_email);
+        userNationalID = findViewById(R.id.user_national_id);
+        signUpBtn = findViewById(R.id.signup_btn);
+
+        //UserProfile
+
+        userProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                } else {
+                    cropImage();
+                }
+            }
+        });
+    }
+
+    private void cropImage() {
     }
 }
